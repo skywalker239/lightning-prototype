bec940a9f5c08b80e8a3abb1cfb28bf03dbd48fa
Alexander Kharitonov <alexander.kharitonov@gmail.com>
2012-02-23 04:13:06 +0400

    Monitor ClientValueQueue statistics.

--- lib/client_value_queue.cc
+++ lib/client_value_queue.cc
@@ -1,15 +1,28 @@
 #include "client_value_queue.h"
 #include <mordor/log.h>
+#include <mordor/statistics.h>
 
 namespace lightning {
 
+using Mordor::CountStatistic;
 using Mordor::FiberMutex;
 using Mordor::FiberEvent;
 using Mordor::Logger;
 using Mordor::Log;
+using Mordor::Statistics;
 using paxos::Value;
 
 static Logger::ptr g_log = Log::lookup("lightning:client_value_queue");
+
+static CountStatistic<uint64_t>& g_pushes =
+    Statistics::registerStatistic("client_value_queue.pushes",
+                                  CountStatistic<uint64_t>());
+static CountStatistic<uint64_t>& g_pops =
+    Statistics::registerStatistic("client_value_queue.pops",
+                                  CountStatistic<uint64_t>());
+static CountStatistic<uint64_t>& g_unpops =
+    Statistics::registerStatistic("client_value_queue.unpops",
+                                  CountStatistic<uint64_t>());
 
 ClientValueQueue::ClientValueQueue()
     : event_(false)
@@ -25,6 +38,7 @@ boost::shared_ptr<Value> ClientValueQueue::pop() {
         boost::shared_ptr<Value> value = *values_.begin();
         values_.pop_front();
         MORDOR_LOG_TRACE(g_log) << this << " pop " << value->valueId;
+        g_pops.increment();
         if(values_.empty()) {
             event_.reset();
         }
@@ -35,6 +49,7 @@ boost::shared_ptr<Value> ClientValueQueue::pop() {
 void ClientValueQueue::push(boost::shared_ptr<Value> value) {
     FiberMutex::ScopedLock lk(mutex_);
     values_.push_back(value);
+    g_pushes.increment();
     event_.set();
     MORDOR_LOG_TRACE(g_log) << this << " push " << value->valueId;
 }
@@ -42,6 +57,7 @@ void ClientValueQueue::push(boost::shared_ptr<Value> value) {
 void ClientValueQueue::push_front(boost::shared_ptr<Value> value) {
     FiberMutex::ScopedLock lk(mutex_);
     values_.push_front(value);
+    g_unpops.increment();
     event_.set();
     MORDOR_LOG_TRACE(g_log) << this << " push_front " << value->valueId;
 }

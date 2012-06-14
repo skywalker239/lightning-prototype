dbe6e065aa7fdeb9513550b06b8a48756d3b4650
Alexander Kharitonov <alexander.kharitonov@gmail.com>
2012-06-14 21:59:30 +0400

    Log ring changes.

--- lib/ring_change_notifier.cc
+++ lib/ring_change_notifier.cc
@@ -1,11 +1,16 @@
 #include "ring_change_notifier.h"
 #include "ring_configuration.h"
+#include <mordor/log.h>
 
 namespace lightning {
 
+using Mordor::Log;
+using Mordor::Logger;
 using std::pair;
 using std::string;
 using std::vector;
+
+static Logger::ptr g_log = Log::lookup("lightning:ring_change_notifier");
 
 RingChangeNotifier::RingChangeNotifier(
     const vector<RingHolder::ptr>& ringHolders)
@@ -15,12 +20,14 @@ RingChangeNotifier::RingChangeNotifier(
 void RingChangeNotifier::onRingChange(
     RingConfiguration::const_ptr newRing) const 
 {
+    MORDOR_LOG_INFO(g_log) << this << " ring change " << *newRing;
     for(size_t i = 0; i < ringHolders_.size(); ++i) {
         ringHolders_[i]->resetRingConfiguration(newRing);
     }
 }
 
 void RingChangeNotifier::onRingDown() const {
+    MORDOR_LOG_INFO(g_log) << this << " ring down";
     for(size_t i = 0; i < ringHolders_.size(); ++i) {
         ringHolders_[i]->resetRingConfiguration(
             RingConfiguration::const_ptr());
